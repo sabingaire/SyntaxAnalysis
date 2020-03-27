03658c9fb3fa81f41a74d19fc70bf5410f776c2b
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-03-27 12:03:49 -0400

    Global declaration

--- front.c
+++ front.c
@@ -6,3 +6,15 @@ arithmetic expressions */
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+
+/* Global declarations */ /* Variables */
+int charClass;
+char lexeme [100];
+char nextChar;
+int lexLen;
+int token;
+int nextToken;
+int lineNumber = 0;
+int currentIndexCount;
+FILE *fp, *fopen();
